465c3df28448c64f77df484180a3df1395c99504
BYUNG <45321943+ilovebyung@users.noreply.github.com>
2023-02-12 23:14:58 +0900

    delete vae

--- augmentation.py
+++ augmentation.py
@@ -1,10 +1,9 @@
 from keras.preprocessing.image import ImageDataGenerator
 import numpy as np
 import cv2
-
-# Define directory
-source = 'D:/data/sonic_source/357.jpeg'
-target = 'D:/data/sonic_augmentation'
+import os
+import glob
+import matplotlib.pyplot as plt
 
 # Define generator
 datagen = ImageDataGenerator(
@@ -16,11 +15,20 @@ datagen = ImageDataGenerator(
     horizontal_flip=True,
     fill_mode='nearest')
 
-img = cv2.imread(source)
-img = img.reshape((1,) + img.shape)
-i = 0
-for batch in datagen.flow(img, batch_size=1,
-                          save_to_dir=target, save_prefix='image', save_format='jpeg'):
-    i += 1
-    if i > 20:
-        break
+# Define working directory
+source = 'D:/data/sonic_source'
+target = 'D:/data/sonic_augmentation'
+
+os.chdir(source)
+files = glob.glob('*.jpeg')
+
+for file in files:
+    # img = cv2.imread(source)
+    img = plt.imread(file)
+    img = img.reshape((1,) + img.shape)
+    i = 0
+    for batch in datagen.flow(img, batch_size=1,
+                              save_to_dir=target, save_prefix='image', save_format='jpeg'):
+        i += 1
+        if i > 20:
+            break
